53e815fbdb949de0bee93360b2a1c72a168bf169
clementparpaillon <clement.parpaillon@viacesi.fr>
2017-06-01 13:49:25 +0200

    Interface IDogfightModel avec méthodes

--- src/dogfightModel/IDogfightModel.java
+++ src/dogfightModel/IDogfightModel.java
@@ -1,5 +1,14 @@
 package dogfightModel;
 
 public interface IDogfightModel {
-
+	public IArea getArea();
+	public void buildArea(Dimension dimension);
+	public void addMobile(IMobile Mobile);
+	public void removeMobile(IMobile Mobile);
+	public ArrayList<IMobile> getMobiles()
+	{
+		
+	}
+	public void setMobileHavesMoved();
+	
 }
