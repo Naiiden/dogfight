aeb6607540b2950e2c94c3990cf6de48ef73b654
clementparpaillon <clement.parpaillon@viacesi.fr>
2017-06-01 13:46:09 +0200

    Classe DogfightModel avec méthodes

--- src/dogfightModel/DogfightModel.java
+++ src/dogfightModel/DogfightModel.java
@@ -1,5 +1,37 @@
 package dogfightModel;
 
 public class DogfightModel {
-
+	public DogfightModel() 
+	{
+		
+	}
+	public IArea getArea()
+	{
+		
+	}
+	public void buildArea(Dimension diemnsion)
+	{
+		
+	}
+	public void addMobile(IMobile Mobile)
+	{
+		
+	}
+	public void removeMobile(IMobile Mobile)
+	{
+		
+	}
+	public ArrayList<IMobile> getMobiles()
+	{
+		
+	}
+	public IMobile getMobileByPlayer(int player)
+	{
+		
+	}
+	public void setMobilesHavesMoved()
+	{
+		
+	}
+	
 }
