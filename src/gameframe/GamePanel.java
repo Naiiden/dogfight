d3bf4fb209700e8a0dbaab72aa7792ece05c6a94
Hugo <Hugo@10.144.50.80>
2017-06-01 14:30:25 +0200

    dzd

--- src/gameframe/GamePanel.java
+++ src/gameframe/GamePanel.java
@@ -1,5 +1,17 @@
 package gameframe;
 
+import java.util.Observable;
+import com.sun.prism.Graphics;
+
 public class GamePanel {
 
+	public GamePanel(IGraphicsBuilder graphicsBuilder) {
+		// TODO Auto-generated constructor stub
+	}
+	public void update(Observable observable) {
+		
+	}
+	public void paintComponent(Graphics graphic){
+		
+	}
 }
