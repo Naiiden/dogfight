1e7bbb085bb59c8bd87e2e52e6614b847369ef05
clementparpaillon <clement.parpaillon@viacesi.fr>
2017-06-01 14:03:31 +0200

    Classe Mobile avec méthodes

--- src/dogfightModel/Mobile.java
+++ src/dogfightModel/Mobile.java
@@ -2,4 +2,71 @@ package dogfightModel;
 
 public class Mobile {
 	private int speed;
+	
+	public Mobile(Direction diection, Position position, Dimension dimension, int speed, String image);
+	public Direction getDirection()
+	{
+		
+	}
+	public void setDirection(Direction direction);
+	public Point getPosition()
+	{
+		
+	}
+	public Dimension getDimension()
+	{
+		
+	}
+	public int getSpeed()
+	{
+		
+	}
+	public int getWidth()
+	{
+		
+	}
+	public int getHeight()
+	{
+		
+	}
+	private void moveUp()
+	{
+		
+	}
+	private void moveRight()
+	{
+		
+	}
+	private void moveDown()
+	{
+		
+	}
+	private void moveLeft()
+	{
+		
+	}
+	public Color getColor()
+	{
+		
+	}
+	public IDogfightModel getDogfightModel()
+	{
+		
+	}
+	public void setDogfightModel(IDogfightModel dogfightModel) 
+	{
+		
+	}
+	public boolean hit()
+	{
+		
+	}
+	public boolean isWeapon()
+	{
+		
+	}
+	public Image getImage()
+	{
+		
+	}
 }
