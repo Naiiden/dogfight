5931ade99b2506badb428f6f7953cb78f91e0e91
Hugo <Hugo@10.144.50.80>
2017-06-02 09:13:42 +0200

    gameframe

--- src/gameframe/GameFrame.java
+++ src/gameframe/GameFrame.java
@@ -1,5 +1,16 @@
 package gameframe;
 
+import java.awt.event.KeyEvent;
+import java.util.Observable;
+
 public class GameFrame {
 
+	public GameFrame(String title, IEventPerformer performer, IGraphicsBuilder graphicBuilder, Observable observable){	
+	}
+	public void keyPressed(KeyEvent keyEvent) {
+	}
+	public void keyReleased(KeyEvent keyEvent){	
+	}
+	public void keyTyped(KeyEvent keyEvent){
+	}
 }
