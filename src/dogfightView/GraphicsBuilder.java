579ec00af51e90cbfb5f0276cee0a855bc675424
Hugo <Hugo@10.144.50.80>
2017-06-02 09:30:33 +0200

    qsdqdq

--- src/dogfightView/GraphicsBuilder.java
+++ src/dogfightView/GraphicsBuilder.java
@@ -1,37 +1,37 @@
 package dogfightView;
 
 import java.awt.Graphics;
+
 import java.awt.image.ImageObserver;
 
 import dogfightModel.IDogfightModel;
 import dogfightModel.IMobile;
 
 public class GraphicsBuilder {
-	public GraphicsBuilder(IDogfightModel dogfightModel)
-	{
+	public GraphicsBuilder(IDogfightModel dogfightModel) {
+
+	}
+
+	private void buildEmptySky() {
+
+	}
+
+	private void drawMobile(final IMobile mobile, final Graphics graphics, final ImageObserver Observer) {
 		
 	}
-	private void buildEmptySky() 
-	{
-		
-	}
-	private void drawMobile(IMobile mobile, Graphics graphics, ImageObserver Observer) 
-	{
-		
-	}
-	
-	public int getGlobalWidth()
-	{
+ 
+	public int getGlobalWidth() {
 		return 1;
-		
+
 	}
-	
-	public int getGlobalHeight()
-	{
+
+	public int getGlobalHeight() {
 		return 1;
-		
+
 	}
+
 	public void applyModelToGraphic(Graphics graphics, ImageObserver observer){
 		
 	}
+
 }
